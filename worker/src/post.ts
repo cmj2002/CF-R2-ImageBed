718b6442458eab5712d86c26d0c23c25eb043d76
Cao Mingjun <me@caomingjun.com>
2023-02-24 18:19:12 +0800

    Add verbose option to `getInfo`

--- worker/src/post.ts
+++ worker/src/post.ts
@@ -6,14 +6,28 @@
 import { getKey, validate } from './utils'
 import { config } from './config'
 
-async function getInfo(key: string): Promise<Response> {
+async function getInfo(key: string, options: any): Promise<Response> {
   const object = await BUCKET.get(key)
   if (object) {
-    const r = {
-      type: 'file',
-      path: key,
-      size: object.size,
-      updated: object.uploaded.toISOString(),
+    let r;
+    if(!options.verbose){
+      r = {
+        type: 'file',
+        path: key,
+        size: object.size,
+        updated: object.uploaded.toISOString(),
+      }
+    }else{
+      r = {
+        type: 'file',
+        path: key,
+        size: object.size,
+        updated: object.uploaded.toISOString(),
+        httpMetadata: object.httpMetadata,
+        customMetadata: object.customMetadata,
+        checksums: object.checksums,
+        etag: object.etag,
+      }
     }
     return new Response(JSON.stringify(r), {
       status: 200,
@@ -22,10 +36,26 @@ async function getInfo(key: string): Promise<Response> {
   } else {
     const folder = await BUCKET.list({ prefix: key })
     if (folder && folder.objects.length > 0) {
+      let files;
+      if(!options.verbose){
+        files = folder.objects.map((o) => o.key)
+      }else{
+        files = folder.objects.map((o) => {
+          return {
+            path: o.key,
+            size: o.size,
+            updated: o.uploaded.toISOString(),
+            httpMetadata: o.httpMetadata,
+            customMetadata: o.customMetadata,
+            checksums: o.checksums,
+            etag: o.etag,
+          }
+        })
+      }
       const r = {
         type: 'folder',
         path: key,
-        files: folder.objects.map((o) => o.key),
+        files: files,
       }
       return new Response(JSON.stringify(r), {
         status: 200,
@@ -91,7 +121,7 @@ export async function handlePOST(request: Request): Promise<Response> {
 
   switch (action) {
     case 'info':
-      return await getInfo(key)
+      return await getInfo(key, json)
     case 'delete':
       return await deleteObject(key)
     default:
